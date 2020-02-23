0339df280a196615b23a2719215d50903bd8338d
Lucas P Torres de Araújo <lucast0rr3s@gmail.com>
2020-02-23 20:32:57 -0300

    Added shebang and coding utf-8

--- bcolors.py
+++ bcolors.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from config import partners
 from bcolors import bcolors
 
